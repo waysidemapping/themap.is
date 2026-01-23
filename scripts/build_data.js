0adb58c9c5291bf11fa51f62acac033607c06503
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-01-22 20:43:46 -0500

    Check which icons need parts

--- scripts/build_data.js
+++ scripts/build_data.js
@@ -1,8 +1,6 @@
 import fs from "fs";
 import path from "path";
 
-let featureTypeDir = "data/presets/";
-
 const presets = {};
 
 const allowedKeys = {
@@ -49,25 +47,21 @@ function processPreset(id, json) {
 
   if (json.icon) {
     if (!fs.existsSync(`icons/${json.icon}.svg`)) {
-      console.log(`Missing icon file ${json.icon} for ${id}`)
+      console.log(`Missing icon file "${json.icon}.svg" for ${id}`)
     }
   }
 
   presets[id] = json;
 }
 
-function walkDir(dir, fileList = []) {
+function walkDir(dir, func) {
   const entries = fs.readdirSync(dir, { withFileTypes: true });
 
   // Process files first
   for (const entry of entries) {
     if (entry.isFile()) {
       const filePath = path.join(dir, entry.name);
-      if (filePath.endsWith(".json")) {
-        const id = filePath.substring(featureTypeDir.length, filePath.length - 5);
-        const json = JSON.parse(fs.readFileSync(filePath));
-        processPreset(id, json);
-      }
+      func(filePath);
     }
   }
 
@@ -75,11 +69,35 @@ function walkDir(dir, fileList = []) {
   for (const entry of entries) {
     if (entry.isDirectory()) {
       const subDir = path.join(dir, entry.name);
-      walkDir(subDir, fileList);
+      walkDir(subDir, func);
     }
   }
-
-  return fileList;
 }
 
-const allFiles = walkDir(featureTypeDir);
+const presetsDir = "data/presets/";
+walkDir(presetsDir, function(filePath) {
+  if (filePath.endsWith(".json")) {
+    const id = filePath.substring(presetsDir.length, filePath.length - 5);
+    const json = JSON.parse(fs.readFileSync(filePath));
+    processPreset(id, json);
+  }
+});
+
+const iconIds = {};
+const iconIdParts = {};
+
+const iconsDir = 'icons/';
+walkDir('icons/', function(filePath) {
+  if (filePath.endsWith(".svg")) {
+    const id = filePath.substring(iconsDir.length, filePath.length - 4);
+    iconIds[id] = true;
+    const parts = id.split('-');
+    if (parts[0] !== id) {
+      parts.forEach(part => iconIdParts[part] = true);
+    }
+  }
+});
+Object.keys(iconIdParts)
+  .sort()
+  .filter(part => !iconIds[part])
+  .forEach(part => console.log(`Missing base icon "${part}"`));
