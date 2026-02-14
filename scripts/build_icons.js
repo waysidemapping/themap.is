174159532bc1a5bc02e83960bdc9d7421563ad1b
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-02-13 20:51:47 -0500

    Update for pinhead

--- scripts/build_icons.js
+++ scripts/build_icons.js
@@ -2,7 +2,7 @@
 import { existsSync, rmSync, mkdirSync, readdirSync, copyFileSync } from 'fs';
 import { join, basename, extname } from 'path';
 
-const sourceDir = './icons';
+const sourceDir = './pinhead-map-icons/svg';
 const targetDir = './dist/icons';
 
 function ensureEmptyDir(dir) {
